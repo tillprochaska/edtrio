7b851d12369c56cd337bd4918696fd6ca281a6ff
Till Prochaska <mail@tillprochaska.de>
2018-12-25 23:41:17 +0100

    Hide plus menu in void blocks

--- src/plugins/plus-menu/index.tsx
+++ src/plugins/plus-menu/index.tsx
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import { Editor } from "slate";
 import PlusMenu from "./PlusMenu";
 
 export default function PlusMenuPlugin() {
@@ -14,12 +13,31 @@ export default function PlusMenuPlugin() {
 }
 
 const addPlusMenuBesideEditor = {
-  renderEditor(props: any, editor: Editor, next: any) {
-    const restOfEditor = next();
+  renderEditor(props: any, editor: any, next: any) {
+    // If the editor currently isn’t focused, or if
+    // no block node is focused, do not render the
+    // plus menu.
+    const { isFocused } = editor.value.selection;
+    const focusedBlock = editor.value.focusBlock;
+
+    // If the focused block is a void block, users won’t
+    // be able to edit its content or add children, thus
+    // the plus menu would be useless.
+    const isVoidBlock = editor.isVoid(focusedBlock);
+
+    // Also, there are some block types where the plus menu
+    // should not be displayed, e. g. the title block.
+    const ignoredBlocks = ['title'];
+    const isIgnoredBlock = ignoredBlocks.includes(focusedBlock.type);
+
+    if(!isFocused || !focusedBlock || isVoidBlock || isIgnoredBlock) {
+      return next();
+    }
+
     return (
       <Fragment>
-        <PlusMenu editor={editor} startBlock={editor.value.startBlock} />
-        {restOfEditor}
+        {<PlusMenu editor={editor} startBlock={editor.value.startBlock} />}
+        {next()}
       </Fragment>
     );
   },
