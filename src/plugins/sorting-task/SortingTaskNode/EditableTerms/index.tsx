ee7089539b13f8e89e3ce8e895d690514d92a61c
Till Prochaska <mail@tillprochaska.de>
2018-12-26 15:50:58 +0100

    Allow user to add new terms by appending empty row to list of terms

--- src/plugins/sorting-task/SortingTaskNode/EditableTerms/index.tsx
+++ src/plugins/sorting-task/SortingTaskNode/EditableTerms/index.tsx
@@ -10,7 +10,7 @@ interface IProps {
 export default class EditableTerms extends React.PureComponent<IProps> {
 
   public render() {
-    const terms = this.props.terms.map((term, index) => {
+    const rows = this.getRows().map((term, index) => {
       return (
         <EditableTermItem
           key={index}
@@ -29,16 +29,47 @@ export default class EditableTerms extends React.PureComponent<IProps> {
           </tr>
         </thead>
         <tbody>
-          {terms}
+          {rows}
         </tbody>
       </table>
     );
+  }
+
+  /*
+   * This method returns a list of terms to render as rows.
+   * If the last row is not empty, it appends an empty row.
+   * This way, users are able to add a new term to the list
+   * without the need to manuall append a row.
+   */
+  protected getRows(): ITermsList {
+    const rows = this.props.terms;
+    const lastRow = rows[rows.length - 1];
+    const lastRowIsEmpty = lastRow && !lastRow.term && !lastRow.description;
+
+    if(!lastRowIsEmpty) {
+      rows.push({
+        term: '',
+        description: '',
+      });
+    }
+
+    return rows;
   }
 
   protected editHandler(index: number) {
     return (updatedTerm: ITerm) => {
       const terms = this.props.terms;
       terms[index] = updatedTerm;
+
+      // As there’s always an empty row append to the end of
+      // the list in order to allow users to add new terms,
+      // we need to remove it here in order to get the actual
+      // list of non-empty terms.
+      const lastTerm = terms[terms.length - 1];
+      if(lastTerm && !lastTerm.term && !lastTerm.description) {
+        terms.pop();
+      }
+
       this.props.onEdit(terms);
     };
   }
