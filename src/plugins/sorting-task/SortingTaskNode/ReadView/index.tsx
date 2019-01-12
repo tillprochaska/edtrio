53a3bf9cce8c0d1b36539804c11cc825145c2595
Usame Algan <usame33@hotmail.de>
2019-01-12 20:41:12 +0100

    extracts flipcard into react component

--- src/plugins/sorting-task/SortingTaskNode/ReadView/index.tsx
+++ src/plugins/sorting-task/SortingTaskNode/ReadView/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ILearningItems } from "../interfaces";
+import FlipCard from "./FlipCard";
 
 import "./style.scss";
 
@@ -14,20 +15,7 @@ export default class ReadView extends React.PureComponent<IProps> {
     .filter(learningItem => learningItem.term && learningItem.description)
     .map((learningItem, index) => {
       return (
-        <React.Fragment key={index}>
-          <li className="flip-card">
-            <div className="flip-card__inner">
-              <div className="flip-card__front flip-card__content">
-                <h2 className="flip-card__title">Kannst du diesen Begriff erklären?</h2>
-                <p className="flip-card__text">{learningItem.term}</p>
-              </div>
-              <div className="flip-card__back flip-card__content">
-                <h2 className="flip-card__title">Erklärung:</h2>
-                <p className="flip-card__text">{learningItem.description}</p>
-              </div>
-            </div>
-          </li>
-        </React.Fragment>
+        <FlipCard key={index} learningItem={learningItem} />
       );
     });
 
