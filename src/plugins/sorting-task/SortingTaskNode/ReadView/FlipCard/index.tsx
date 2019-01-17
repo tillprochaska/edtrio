3661c0b45ccb62fc6e5e6878d475e19b2a4fc566
Adrian Jost <adrianjost@users.noreply.github.com>
2019-01-17 16:35:23 +0100

    nextCard is working

--- src/plugins/sorting-task/SortingTaskNode/ReadView/FlipCard/index.tsx
+++ src/plugins/sorting-task/SortingTaskNode/ReadView/FlipCard/index.tsx
@@ -4,25 +4,60 @@ import { ILearningItem } from "../../interfaces";
 import "./style.scss";
 
 interface IProps {
-    learningItem: ILearningItem,
-};
+	learningItem: ILearningItem;
+	nextCard: (answerKnown: boolean) => () => void,
+}
 
-export default class FlipCard extends React.PureComponent<IProps> {
+interface IState {
+	isFlipped: boolean;
+}
 
-    public render() {
-        return (
-            <li className="flip-card">
-                <div className="flip-card__inner">
-                    <div className="flip-card__front flip-card__content">
-                        <h2 className="flip-card__title">Kannst du diesen Begriff erklären?</h2>
-                        <p className="flip-card__text">{this.props.learningItem.term}</p>
-                    </div>
-                    <div className="flip-card__back flip-card__content">
-                        <h2 className="flip-card__title">Erklärung:</h2>
-                        <p className="flip-card__text">{this.props.learningItem.description}</p>
-                    </div>
-                </div>
-            </li>
-        );
-    }
+export default class FlipCard extends React.Component<IProps, IState> {
+
+	constructor(props: IProps) {
+		super(props);
+		this.state = {
+			isFlipped: false,
+		};
+	}
+
+	public render() {
+		return (
+			<li className="flip-card">
+				<div
+					className={`flip-card__inner ${
+						this.state.isFlipped ? "flip-card--is-flipped" : ""
+					}`}
+				>
+					<div className="flip-card__front flip-card__content">
+						<h2 className="flip-card__title">
+							Kannst du diesen Begriff erklären?
+						</h2>
+						<p className="flip-card__text">{this.props.learningItem.term}</p>
+						<button type="button" onClick={this.showSolution()}>
+							Auflösen
+						</button>
+					</div>
+					<div className="flip-card__back flip-card__content">
+						<h2 className="flip-card__title">Erklärung:</h2>
+						<p className="flip-card__text">
+							{this.props.learningItem.description}
+						</p>
+						<button type="button" onClick={this.props.nextCard(true)}>
+							JA
+						</button>
+						<button type="button" onClick={this.props.nextCard(false)}>
+							NEIN
+						</button>
+					</div>
+				</div>
+			</li>
+		);
+	}
+
+	protected showSolution() {
+		return () => {
+			this.setState({ isFlipped: true });
+		}
+	}
 }
--- src/plugins/sorting-task/SortingTaskNode/ReadView/index.tsx
+++ src/plugins/sorting-task/SortingTaskNode/ReadView/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ILearningItems } from "../interfaces";
+import { ILearningItem, ILearningItems } from "../interfaces";
 import FlipCard from "./FlipCard";
 
 import "./style.scss";
@@ -8,18 +8,51 @@ interface IProps {
   learningItems: ILearningItems,
 };
 
-export default class ReadView extends React.PureComponent<IProps> {
+interface ILearningItemsWithKnownState extends Array<ILearningItemWithKnownState> {};
+interface ILearningItemWithKnownState {
+  term: string,
+  description?: string,
+  isSolved: boolean,
+  wasViewed: boolean
+};
+
+interface IState {
+	learningItems: ILearningItemsWithKnownState,
+}
+
+export default class ReadView extends React.Component<IProps, IState> {
+
+  constructor(props: IProps) {
+		super(props);
+    this.state = { learningItems: this.props.learningItems.map((learningItem: ILearningItem) => ({...learningItem, isSolved: false, wasViewed: false})) }
+  }
 
   public render() {
-    const learningItems = this.props.learningItems
-    .filter(learningItem => learningItem.term && learningItem.description)
+    const learningItems = this.state.learningItems
     .map((learningItem, index) => {
-      return (
-        <FlipCard key={index} learningItem={learningItem} />
-      );
+      return (learningItem.term && learningItem.description && !learningItem.isSolved && !learningItem.wasViewed) ?
+        (
+          <FlipCard key={index} learningItem={learningItem} nextCard={this.nextCard(index)} />
+        )
+      :
+        <React.Fragment key={index}/>
     });
 
     return <ol className="learning-items__cards">{learningItems}</ol>;
   }
+
+  protected nextCard(key: number) {
+    return (answerKnown: boolean) => {
+      return () => {
+        const learningItems = this.state.learningItems;
+        learningItems[key] = {
+          ...learningItems[key],
+          isSolved: answerKnown,
+          wasViewed: true
+        };
+        this.setState( { learningItems } );
+      }
+    }
+	}
 
 }
